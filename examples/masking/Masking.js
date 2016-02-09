60723834068edc7d666d9172414373fd1d195333
Andrew Start <start.andrew@gmail.com>
2016-02-09 11:15:05 -0500

    Added test tweening to Masking example.

--- examples/masking/Masking.js
+++ examples/masking/Masking.js
@@ -1,13 +1,17 @@
 (function (lib) {
 
-var MovieClip = PIXI.Container;
+var MovieClip = PIXI.animate.MovieClip;
 var Graphics = PIXI.Graphics;
 var graphics = PIXI.animate.GraphicsCache;
 
 // stage content:
 lib.Masking = MovieClip.extend(function(mode, startPosition, loop)
 {
+	if (loop === undefined)
+		loop = true;
 	MovieClip.call(this, mode, startPosition, loop);
+	
+	this._frameDuration = 25;
 
 	// Layer 2 (mask)
 	var mask_graphics_0 = new Graphics()
@@ -15,7 +19,7 @@ lib.Masking = MovieClip.extend(function(mode, startPosition, loop)
 		
 	var mask_graphics_12 = new Graphics()
 		.d(graphics.mask_graphics_12);
-
+	
 	// this.timeline.addTween(
 	// 	Tween.get(this)
 	// 		.to({ mask: mask_graphics_0 })
@@ -30,13 +34,15 @@ lib.Masking = MovieClip.extend(function(mode, startPosition, loop)
 		.ma(mask_graphics_0);
 
 	// this.timeline.addTween(Tween.get(shape).wait(25));
+	
+	this.tw(this.shape, {x:75, y:72}, 0, 12).tw(this.shape, {x:100, y:300}, 12, 25);
+	//this.tw(this.shape, {m: mask_graphics_12}, 0, 12);
 
 	this.ac(
 		this.shape,
 		mask_graphics_0,
 		mask_graphics_12
 	);
-
 });
 
 })(lib = lib||{});
