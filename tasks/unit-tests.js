e3a47471e6c96fe4010a5bf4be6b8ae3f568be8e
Matt Karl <matt.karl@jibo.com>
2016-10-11 10:47:01 -0400

    Adds more documentation for play sound, house-keeping

--- tasks/unit-tests.js
+++ tasks/unit-tests.js
@@ -1,10 +1,14 @@
-"use strict";
-
 module.exports = function(gulp, options, plugins) {
     gulp.task('unit-tests', function(done) {
-        plugins.floss.run({
+        var flossOptions = {
             path: 'tests/index.js',
-            debug: !!(options.argv && options.argv.debug)
-        }, done)
+            debug: !!options.argv.debug
+        };
+        if (options.argv.coverage) {
+            flossOptions.coveragePattern = 'dist/pixi-animate.js';
+            flossOptions.coverageSourceMaps = true;
+            flossOptions.coverageHtmlReporter = true;
+        }
+        plugins.floss(flossOptions, done);
     });
 };
