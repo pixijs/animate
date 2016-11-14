dfe3db1e2dc05c12b166d5c2f174500799aaf76e
Matt Karl <matt.karl@jibo.com>
2016-11-14 15:40:12 -0500

    removes new sprite sheet format

--- src/animate/index.js
+++ src/animate/index.js
@@ -3,7 +3,6 @@ import sound from './sound';
 import utils from './utils';
 import MovieClip from './MovieClip';
 import ShapesCache from './ShapesCache';
-import SpritesheetLoader from './SpritesheetLoader';
 import SymbolLoader from './SymbolLoader';
 import Timeline from './Timeline';
 import Tween from './Tween';
@@ -21,7 +20,6 @@ export default {
     utils,
     MovieClip,
     ShapesCache,
-    SpritesheetLoader,
     SymbolLoader,
     Timeline,
     Tween
