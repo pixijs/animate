46af8aa5346ed24c1aa34956354052396c900737
Matt Karl <matt.karl@jibo.com>
2016-11-21 17:01:55 -0500

    Added the typings, PIXI getter mixin

--- src/index.js
+++ src/index.js
@@ -1,20 +1,18 @@
-// Export for Node-compatible environments like Electron
-if (typeof module !== 'undefined' && module.exports) {
-    // Attempt to require the pixi module
-    if (typeof PIXI === 'undefined') {
-        require('pixi.js');
-    }
-    // Export the module
-    module.exports = require('./animate').default;
-}
 // If we're in the browser make sure PIXI is available
-else if (typeof PIXI === 'undefined') {
+if (typeof PIXI === 'undefined') {
     throw "Requires PIXI";
 }
 
 // Include the PIXI mixins
 require('./mixins');
 
-// Assign to global namespace
-PIXI.animate = require('./animate').default;
-PIXI.animate.VERSION = '/* @echo VERSION */' || '';
+// Add to the PIXI global object
+Object.defineProperty(PIXI, 'animate', {
+    enumerable: true,
+    get() { return require('./animate'); }
+});
+
+// Export for Node-compatible environments like Electron
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = PIXI.animate;
+}
