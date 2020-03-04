be0b39fe169891bb9452da7e2df00ce6b7c51884
andrewstart <start.andrew@gmail.com>
2020-03-04 15:33:26 -0500

    Rename 'mixin' classes on library export for nicer exports.

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
 export * from './animate';
-export * from './mixins';
+export {
+	AnimateDisplayObject as DisplayObject,
+	AnimateContainer as Container,
+	AnimateText as Text,
+	AnimateGraphics as Graphics,
+	AnimateSprite as Sprite
+} from './mixins';
