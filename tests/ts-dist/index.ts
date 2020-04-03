d118d538439ea35441cc1249212c1b8ccc17e53b
andrewstart <start.andrew@gmail.com>
2020-04-02 21:43:02 -0400

    Add tests to confirm that the library definitions aren't broken.

---
+++ tests/ts-dist/index.ts
@@ -0,0 +1,7 @@
+/* eslint-disable @typescript-eslint/triple-slash-reference, spaced-comment */
+/// <reference path="../../node_modules/pixi.js/pixi.js.d.ts" />
+/// <reference path="../../dist/pixi-animate-ambient.d.ts" />
+
+const clip = new PIXI.animate.MovieClip();
+
+clip.play();
